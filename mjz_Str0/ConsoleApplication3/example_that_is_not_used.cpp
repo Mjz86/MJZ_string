e29de80fe415411bc53839531a706f66b544a1a9
Mjz86 <112626877+Mjz86@users.noreply.github.com>
2023-11-20 10:21:17 +0330

    u

--- mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
+++ mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
@@ -28,5 +28,5 @@ std::unique_ptr<main_class> main_class::run(int, const char* const* const) {
 int my_main::main(int argc, const char* const* const argv) {
   using namespace mjz_ard::have_mjz_ard_removed;
 
- return 0;
+  return 0;
 }
