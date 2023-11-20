6c7989043db1b78295b89c2d4b327fbbf92cbf98
Mjz86 <112626877+Mjz86@users.noreply.github.com>
2023-11-20 12:01:14 +0330

    Update example_that_is_not_used.cpp

--- mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
+++ mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
@@ -27,22 +27,6 @@ std::unique_ptr<main_class> main_class::run(int, const char* const* const) {
 #include "fstream"
 int my_main::main(int argc, const char* const* const argv) {
   using namespace mjz_ard::have_mjz_ard_removed;
-  ;
-  heap_obj_warper<mjz_Str> str = mjz_Str(" hi mom i am a string ")(
-      [](mjz_Str& obj,
-         heap_obj_warper<mjz_Str>&&  val) -> heap_obj_warper<mjz_Str> {
-        val.init(std::move(obj));
-	return val; 
-      }, heap_obj_warper<mjz_Str>());
-  std::cout << str <<'\n';
-  mjz_ard::iterator_template<char> it((char*)str.pointer_to_unsafe_data_buffer(),
-                                      str.size);
-  for (auto c: it) {
-    bool c_is_null = (c == 0);
-    std::cout << (char)((c & MJZ_logic_BL_bit_to_64_bits(!c_is_null)) |
-                  ('?'& MJZ_logic_BL_bit_to_64_bits(c_is_null)));
-  }
-  std::cout << '\n';
   
 
   return 0;
