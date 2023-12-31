21941a3ab3bc7e09ee2bf03a69b7224ef682a403
Mjz86 <112626877+Mjz86@users.noreply.github.com>
2023-12-31 18:33:55 +0330

    Update example_that_is_not_used.cpp

--- mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
+++ mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
@@ -5,11 +5,6 @@ int my_main::main(int argc, const char *const *const argv) {
   using namespace mjz;
   using namespace mjzt;
   using namespace mjz::ssco;
-  int i{}, j{};
-  std::cin >> i>>j;
-  mjz_str s = mjz_str::ULL_LL_to_str(i, j, 1, 0);
-  println(s);
-
 
 
   return 0;
