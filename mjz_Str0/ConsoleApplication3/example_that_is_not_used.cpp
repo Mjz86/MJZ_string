58a6fa87ef8ef55c3c9566421cfd4a7722c59ef1
Mjz86 <112626877+Mjz86@users.noreply.github.com>
2023-12-31 18:32:11 +0330

    made the string bug go

--- mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
+++ mjz_Str0/ConsoleApplication3/example_that_is_not_used.cpp
@@ -5,7 +5,10 @@ int my_main::main(int argc, const char *const *const argv) {
   using namespace mjz;
   using namespace mjzt;
   using namespace mjz::ssco;
-
+  int i{}, j{};
+  std::cin >> i>>j;
+  mjz_str s = mjz_str::ULL_LL_to_str(i, j, 1, 0);
+  println(s);
 
 
 
