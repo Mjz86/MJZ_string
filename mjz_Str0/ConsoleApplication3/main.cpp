8d9f80eb724b2682a092373c7c9743a5e96ff0e8
Mjz86 <112626877+Mjz86@users.noreply.github.com>
2024-02-01 19:28:47 +0330

    Update main.cpp

--- mjz_Str0/ConsoleApplication3/main.cpp
+++ mjz_Str0/ConsoleApplication3/main.cpp
@@ -3,42 +3,31 @@
 namespace mjz_ard {}  // namespace mjz_ard
 /*
  *for some unknown reason the auto complete doesn't work on my msvc for big
- *classes like the optional  . PLANS : the error part of the optional is in
- *construction and may take a while to complete because of the mjz optional
- *complexity and powerful unsafe-safe  user API functions .(aka a good and
- *featureful type safe union of {Type,Error_t}+1byte+padding )
- *
- *time is limited so  the minimal  mjz string should wait a bit  for the
- *optional / Result  class to complete and then for the revamp of the allocators
+ *classes like the optional  . PLANS :
+ *time is limited so  the minimal  mjz string should wait a bit  for 
+ *the revamp of the allocators
  *and some mess that should be cleaned and then for the vectors and the classes
  *dependent on dynamic allocators to update api and finally then the minimal mjz
  *string will be written
  * and the unique and sheared and weak ptr will be next
  */
-
-auto rust_like_function(std::string_view view)
-    -> mjzt::Result<int, std::string> {
-  if (view.empty()) return {mjzt::erropt, "no input"};
-  char answer = view[0];
-  if (answer == 'Y' || answer == 'y') return {100};
-  if (answer == 'N' || answer == 'n') return mjzt::valopt;
-  std::string err= "bad input:" + std::string(view);
-  return {mjzt::erropt, err};
-}
 int my_main::main(int argc, const char* const* const argv) {
   USE_MJZ_NS();
-  std::string s;
-  println("give some input[Y/N]:");
-  scan(s);
-  if (s == "@E") s = "";
-  if (auto o = rust_like_function(s); !!o) {
-    print("success",' ', *o, ' ');
-    println( ++o());
-  } else if(o.has_error()){
-    println("Error:", o.error());
-  } else {
-   println("Unknown Error");
-  }
-  println("end");
+  result<operation_reporter> r = valopt; 
+  /*
+  * why i hate padding :
+  * this could be half the size
+  * (char*)&r :
+  * [0] operation_reporter padding / char (Error_t)
+  * [1] operation_reporter char
+  * [2] result uint8_t (state)
+  * [3] result padding
+  */
+  char*p=r.uuep();
+  println( *r);
+  assert(sizeof(operation_reporter) == 2);
+  p[0] = '%';// padding  in here
+  p[1] = '#';// inner char  in here
+  println(*r);
   return 0;
 }
